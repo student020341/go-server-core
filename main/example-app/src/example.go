16b323d42e6a0ebdaef61c65161078efb8237c5b
Tom Esposito <student020341@gmail.com>
2021-11-26 23:42:02 +0000

    use basic plugin in example

--- main/example-app/src/example.go
+++ main/example-app/src/example.go
@@ -9,14 +9,14 @@ import (
 	"github.com/student020341/go-server-core/TWTServer"
 )
 
-// router included with server core
-var router TWTServer.SubRouter
+// using basic plugin provided by TWTServer, includes subrouter
+var Foo TWTServer.BasicPlugin
 
 // use init to setup your router
 func init() {
 	// www.site.com/misc/file/anything.asdf
 	// shows route that takes a writer, a request, and args - expected to respond to client directly
-	router.Register("/file/*", "GET", func(w http.ResponseWriter, r *http.Request, args map[string]interface{}) {
+	Foo.Router.Register("/file/*", "GET", func(w http.ResponseWriter, r *http.Request, args map[string]interface{}) {
 		// will attempt to serve the given path
 		// grab the length of (/name/file/) and pass along the rest to the file path
 		// files/misc/hello.html will be included with the base repo, visit www.yoursite.com/name/file/hello.html to check it out
@@ -24,14 +24,14 @@ func init() {
 	})
 
 	// server particular file from exact without using file/* strategy
-	router.Register("/foo", "GET", func(w http.ResponseWriter, r *http.Request, args map[string]interface{}) {
+	Foo.Router.Register("/foo", "GET", func(w http.ResponseWriter, r *http.Request, args map[string]interface{}) {
 		http.ServeFile(w, r, fmt.Sprintf("%s/hello.html", Foo.FileDir))
 	})
 
 	// www.site.com/misc/code/200
 	// shows a route that takes args only - expected to return a json-like interface to be returned to client as json
 	// note: needs to be a real status code or the server will throw errors
-	router.Register("/code/:code", "*", func(args map[string]interface{}) interface{} {
+	Foo.Router.Register("/code/:code", "*", func(args map[string]interface{}) interface{} {
 		// get route arguments
 		route := args["route"].(map[string]string)
 		// get :code from route arguments
@@ -55,7 +55,7 @@ func init() {
 	})
 
 	// 404 route -- register last :)
-	router.Register("*", "*", func(args map[string]interface{}) interface{} {
+	Foo.Router.Register("*", "*", func(args map[string]interface{}) interface{} {
 		return map[string]interface{}{
 			"HTTPStatusCode": 404,
 			"something":      "This is a 404 message as json instead of a file",
